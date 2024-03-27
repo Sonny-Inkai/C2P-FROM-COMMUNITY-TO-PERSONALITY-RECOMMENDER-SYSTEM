866700e6ea5f818b4f0d98040fd9e964caf0aa48
Sonny <126247253+Sonny-Inkai@users.noreply.github.com>
2024-03-27 23:38:31 +0700

    eda data

---
+++ model/main.py
@@ -0,0 +1,13 @@
+mport argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-output_dir', type=str, default=None, help="Path to the output directory.")
+
+    opt = parser.parse_args()
+
+    if opt.output_dir is not None:
+        print(opt.output_dir)
+
+if __name__ == "__main__":
+    main()
